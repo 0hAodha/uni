38b9e2f2b2944f98e59dbc98e94179e116173120
Andrew <andreasohaodha@proton.me>
2024-09-27 21:27:30 +0100

    [CT404]: Lint Week 3 lecture examples

--- year4/semester1/CT404: Graphics & Image Processing/materials/week3/examples/orientation-interpolator.js
+++ year4/semester1/CT404: Graphics & Image Processing/materials/week3/examples/orientation-interpolator.js
@@ -11,29 +11,29 @@ function orientationInterpolator(data) {
 
 orientationInterpolator.prototype.getEulerAngles = function() {
     var now = Date.now();
-    var elapsed = now-this.startTime;
-    while (elapsed>=this.data.times[this.keyframe2]) {
+    var elapsed = now - this.startTime;
+    while (elapsed >= this.data.times[this.keyframe2]) {
         this.keyframe1++;
         this.keyframe2++;
-        if (this.keyframe2>=this.data.times.length) { // loop back to keyframe 0
+        if (this.keyframe2 >= this.data.times.length) { // loop back to keyframe 0
             var timeSinceLastFrame = elapsed - this.data.times[this.keyframe1];
             this.startTime = now - timeSinceLastFrame;
             this.keyframe1 = 0;
-            this.keyframe2 = 1;   
-            elapsed = now-this.startTime;        
+            this.keyframe2 = 1;
+            elapsed = now - this.startTime;
         }
     }
 
     var timeBetweenKeyFrames = this.data.times[this.keyframe2] - this.data.times[this.keyframe1];
     var timeSinceKeyframe1 = elapsed - this.data.times[this.keyframe1];
-    var fractionBetweenFrames = timeSinceKeyframe1/timeBetweenKeyFrames;
+    var fractionBetweenFrames = timeSinceKeyframe1 / timeBetweenKeyFrames;
     var frame1rot = this.data.angles[this.keyframe1];
     var frame2rot = this.data.angles[this.keyframe2];
 
     var rot = {
-        x: frame1rot.x*(1-fractionBetweenFrames) + frame2rot.x*fractionBetweenFrames,
-        y: frame1rot.y*(1-fractionBetweenFrames) + frame2rot.y*fractionBetweenFrames,
-        z: frame1rot.z*(1-fractionBetweenFrames) + frame2rot.z*fractionBetweenFrames
+        x: frame1rot.x * (1 - fractionBetweenFrames) + frame2rot.x * fractionBetweenFrames,
+        y: frame1rot.y * (1 - fractionBetweenFrames) + frame2rot.y * fractionBetweenFrames,
+        z: frame1rot.z * (1 - fractionBetweenFrames) + frame2rot.z * fractionBetweenFrames
     };
 
     return rot;
