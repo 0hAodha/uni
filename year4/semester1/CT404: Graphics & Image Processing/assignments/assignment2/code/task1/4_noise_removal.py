ca71d634e5c99691f9a77afe3c6f25717b748b58
Andrew <andreasohaodha@proton.me>
2024-11-08 01:05:27 +0000

    [CT404]: Assignment 2 progress

--- year4/semester1/CT404: Graphics & Image Processing/assignments/assignment2/code/task1/4_noise_removal.py
+++ year4/semester1/CT404: Graphics & Image Processing/assignments/assignment2/code/task1/4_noise_removal.py
@@ -5,7 +5,7 @@ import cv2
 image = cv2.imread("./output/otsu.jpg", cv2.IMREAD_GRAYSCALE)
 
 # try several different sizes of structuring element (must be odd)
-for kernel_size in range(1, 16, 2):
+for kernel_size in range(1, 32, 2):
     # define a disk-shaped structuring element
     structuring_element = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
 
