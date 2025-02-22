00ee323fe5e5cd6171283ad1a73180ee3afa762a
Andrew <andreasohaodha@proton.me>
2025-02-22 01:48:24 +0000

    [CT420]: Add ./assignments/assignment1/code/query.sh

---
+++ year4/semester2/CT420/assignments/assignment1/code/query.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+file="results.tsv"
+printf "timestamp\tremote\trefid\tst\tt\twhen\tpoll\treach\tdelay\toffset\tjitter\n" > "$file"  # add headings to results file
+
+# every 20 minutes for 8 hours
+for i in $(seq 1 24); do
+    # add the ntpq peers data in TSV format to file with timestamp prepended
+    ntpq --peers | awk '
+        (NR > 2 ) {
+            timestamp = systime();
+            gsub(/ +/, "\t");
+            print(timestamp "\t" $0);
+        }
+    ' >> "$file"
+
+    # do nothing for 20 minutes
+    sleep 20m
+done
